24fa96f135b23ec053184efcbd1eed3e379087ef
AchingShef <achingshef@gmail.com>
2018-09-04 19:43:02 +0300

    finish package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path"),
     HtmlWebPackPlugin = require("html-webpack-plugin"),
-    webpack = require('webpack');
+    // webpack = require('webpack'),
+    CleanWebpackPlugin = require("clean-webpack-plugin"),
+    outputDirectory = "dist";
  
 module.exports = {
     entry: "./src/client/app.jsx",
     output: {
-        path: path.resolve(__dirname, "./dist"),
-        publicPath: "/dist/",
+        path: path.resolve(__dirname, outputDirectory),
         filename: "bundle.js"
     },
     module: {
@@ -17,21 +18,26 @@ module.exports = {
             options: {
                 presets: ["@babel/preset-env","@babel/preset-react"]
             }
-        }, {
+        }, 
+        {
             test: /\.html$/,
             use: [{
                 loader: "html-loader"
             }]
-        }]
+        }
+    ]
     },
     devServer: {
-        contentBase: './dist',
-        hot: true
-    },
+        port: 3000,
+        open: true,
+        proxy: {
+          '/api': 'http://localhost:8080'
+        }
+      },
     plugins: [new HtmlWebPackPlugin({
             template: "./src/client/index.html",
             filename: "./index.html"
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new CleanWebpackPlugin([outputDirectory])
     ]
 };
